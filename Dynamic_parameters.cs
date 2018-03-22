0462c8097a55a0a7c50dd5bd0734d195e9540fe3
Sasha <sasha7692@gmail.com>
2018-03-22 17:16:19 +0200

    Add files via upload

---
+++ Dynamic_parameters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Congruential_generator
+{
+    class Dynamic_parameters
+    {
+        Random rand = new Random();
+        // Substitution of dynamic parameters
+        public void dynamicParametrs(TextBox parametr_A_1_Box, TextBox parametr_A_2_Box, TextBox parametr_C_1_Box, TextBox parametr_C_2_Box, TextBox parametr_M_1_Box, TextBox parametr_M_2_Box)
+        {
+            int[] arrA = { 84589, 36261, 17221, 2416, 4096 };
+            int[] arrC = { 45989, 66037, 107839, 374441, 150889 };
+            int[] arrM = { 1048423, 1048433, 1048447, 1048507, 1048517, 1048549, 1048559, 1048571, 1048573, 1048575 };
+            
+            
+            int randA_1_and_C_1 = rand.Next(0, arrA.Length);
+            int randA_2_and_C_2 = rand.Next(0, arrA.Length);
+            int randM_1 = rand.Next(0, arrM.Length);
+            int randM_2 = rand.Next(0, arrM.Length);
+
+            parametr_A_1_Box.Text = arrA[randA_1_and_C_1].ToString();
+            parametr_A_2_Box.Text = arrA[randA_2_and_C_2].ToString();
+            parametr_C_1_Box.Text = arrC[randA_1_and_C_1].ToString();
+            parametr_C_2_Box.Text = arrC[randA_2_and_C_2].ToString();
+            parametr_M_1_Box.Text = arrM[randM_1].ToString();
+            parametr_M_2_Box.Text = arrM[randM_2].ToString();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Congruential_generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Algorithm conrguentAlgorithm = new Algorithm();
+        Dynamic_parameters dynamicParametrs = new Dynamic_parameters();
+
+        private int x_1; // X0 for Generator 1
+        private int x_2; // X0 for Generator 2
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            x_1 = int.Parse(parametr_X0_1_Box.Text); // Set X0 for Generator 1 
+            x_2 = int.Parse(parametr_X0_2_Box.Text); // Set X0 for Generator 2
+        }
+
+        private void parametr_X0_1_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try { x_1 = int.Parse(parametr_X0_1_Box.Text); } // Set X0 for Generator 1
+            catch { }
+        }
+
+        private void parametr_X0_2_Box_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try { x_2 = int.Parse(parametr_X0_2_Box.Text); } // Set X0 for Generator 2
+            catch { }
+        }
+
+        private void generationButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < int.Parse(NumberOfIterationsBox.Text); i++)
+            {
+                if (DynamicParametersRadioButton.IsChecked == true)
+                {
+                    dynamicParametrs.dynamicParametrs(parametr_A_1_Box, parametr_A_2_Box, parametr_C_1_Box, parametr_C_2_Box, parametr_M_1_Box, parametr_M_2_Box);
+                }
+                x_1 = conrguentAlgorithm.CongruentialGeneratorAlgorithm(x_1, parametr_A_1_Box, parametr_C_1_Box, parametr_M_1_Box);
+                x_2 = conrguentAlgorithm.CongruentialGeneratorAlgorithm(x_2, parametr_A_2_Box, parametr_C_2_Box, parametr_M_2_Box);
+                sequenceTextBox.Text += conrguentAlgorithm.XORAlgorithm(conrguentAlgorithm.DecimalToBinaryAlgorithm(int.Parse(parametr_M_1_Box.Text), x_1), conrguentAlgorithm.DecimalToBinaryAlgorithm(int.Parse(parametr_M_2_Box.Text), x_2));
+            }
+        }
+
+        private void DynamicParametersRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            var bc = new BrushConverter();
+
+            parametr_X0_1_Box.IsReadOnly = parametr_X0_2_Box.IsReadOnly = true;
+            parametr_A_1_Box.IsReadOnly = parametr_A_2_Box.IsReadOnly = true;
+            parametr_C_1_Box.IsReadOnly = parametr_C_2_Box.IsReadOnly = true;
+            parametr_M_1_Box.IsReadOnly = parametr_M_2_Box.IsReadOnly = true;
+
+            parametr_X0_1_Box.Background = parametr_X0_2_Box.Background = (Brush)bc.ConvertFrom("#E8E7E7");
+            parametr_A_1_Box.Background = parametr_A_2_Box.Background = (Brush)bc.ConvertFrom("#E8E7E7");
+            parametr_C_1_Box.Background = parametr_C_2_Box.Background = (Brush)bc.ConvertFrom("#E8E7E7");
+            parametr_M_1_Box.Background = parametr_M_2_Box.Background = (Brush)bc.ConvertFrom("#E8E7E7");
+        }
+
+        private void StaticParametrsRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            parametr_X0_1_Box.IsReadOnly = parametr_X0_2_Box.IsReadOnly = false;
+            parametr_A_1_Box.IsReadOnly = parametr_A_2_Box.IsReadOnly = false;
+            parametr_C_1_Box.IsReadOnly = parametr_C_2_Box.IsReadOnly = false;
+            parametr_M_1_Box.IsReadOnly = parametr_M_2_Box.IsReadOnly = false;
+
+            parametr_X0_1_Box.Background = parametr_X0_2_Box.Background = Brushes.White;
+            parametr_A_1_Box.Background = parametr_A_2_Box.Background = Brushes.White;
+            parametr_C_1_Box.Background = parametr_C_2_Box.Background = Brushes.White;
+            parametr_M_1_Box.Background = parametr_M_2_Box.Background = Brushes.White;
+        }
+    }
+}
